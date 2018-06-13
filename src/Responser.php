14ed034d07f485076b62da487a01938b3d8190a0
Kane <52653784@qq.com>
2018-06-13 10:34:43 +0800

    create

---
+++ src/Responser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Peak\Laravel\Http;
+
+/**
+ * Provide response generating functions
+ * typically used in various situtaions
+ */
+class Responser {
+
+
+	use ResponserComponent;
+
+
+}
