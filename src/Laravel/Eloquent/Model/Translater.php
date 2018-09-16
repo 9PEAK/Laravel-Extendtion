302095104985cf8eadd46d647612e2bb3e5dedd4
Kane <52653784@qq.com>
2018-09-16 14:40:09 +0800

    Translater 新版本

--- src/Laravel/Eloquent/Model/Translater.php
+++ src/Laravel/Eloquent/Model/Translater.php
@@ -1,50 +1,23 @@
 <?php
 
-namespace Peak\Laravel\Eloquent\Model ;
+namespace Peak\Laravel\Eloquent\Model;
 
 trait Translater {
 
-	/**
-	 * 模型翻译的属性名
-	 * */
-	protected static $translation_key = 'translation';
+	use Property;
 
 	/**
-	 * 检测翻译属性是否已设置
+	 * 模型转化翻译的属性名
+	 * */
+	protected static $model_translation = 'translation';
+
+	/**
+	 * 检测model
 	 * */
 	private static function translation_defined():bool
 	{
-		return property_exists(__CLASS__, static::$translation_key);
+		return property_exists(__CLASS__, static::$model_translation);
 	}
-
-	/**
-	 * 获取属性列表
-	 * */
-	public static function translationList ($key)
-	{
-		if (!self::translation_defined()) return false;
-		return @static::$translation_key[$key];
-	}
-
-
-	/**
-	 * 翻译或者编码属性
-	 * */
-	public function translateProperty ($key, $val=null)
-	{
-		if (!$key) {
-			return false;
-		}
-
-		if (isset($val)) {
-			return array_search($val, self::translationList($key) ?: []);
-		}
-
-		return @self::translationList($key)[$this->$key];
-
-	}
-
-
 
 	/**
 	 * [level-3] 转化指定的Model属性
@@ -54,19 +27,23 @@ trait Translater {
 	 * @ext 转化属性名后缀，默认为''
 	 * @return 成功true；如果Model未指定translate属性名，则返回false。
 	 * */
-	public function translateToNewProperty($prop=null, $prf='_', $ext=''):bool
+	public function translateProperty($prop=null, $prf='_', $ext=''):bool
 	{
 		if ( !self::translation_defined()) return false;
 
 		if ( $prop ) {
 			$prop = is_array($prop) ? $prop : explode(',', $prop);
-			$prop = array_intersect($prop, $this->{static::$translation_key});
+			$prop = array_intersect($prop, $this->{static::$model_translation});
 		} else {
-			$prop = $this->{static::$translation_key};
+			$prop = $this->{static::$model_translation};
 		}
 
-		foreach ( $prop as $k ) {
-			$this->{$prf.$k.$ext} = $this->translateProperty($k);
+		if ($prop) {
+			foreach ( $prop as $k ) {
+				if ( $k ) {
+					$this->{$prf.$k.$ext} = $this->getProperty($k);
+				}
+			}
 		}
 
 		return true;
