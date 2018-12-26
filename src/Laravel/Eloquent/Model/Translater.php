59d1b73a641c8e66abb426141bc92566d22b99c9
Kane <52653784@qq.com>
2018-12-26 17:23:54 +0800

    # 方法命名调整 更加方便记忆使用 # 取消多余的translation变量名定义

--- src/Laravel/Eloquent/Model/Translater.php
+++ src/Laravel/Eloquent/Model/Translater.php
@@ -5,16 +5,11 @@ namespace Peak\Laravel\Eloquent\Model ;
 trait Translater {
 
 	/**
-	 * 模型翻译的属性名
-	 * */
-	protected static $translation_key = 'translation';
-
-	/**
 	 * 检测翻译属性是否已设置
 	 * */
 	private static function translation_defined():bool
 	{
-		return property_exists(static::class, static::$translation_key);
+		return property_exists(static::class, 'translation');
 	}
 
 	/**
@@ -23,13 +18,12 @@ trait Translater {
 	public static function translationList ($key)
 	{
 		if (!self::translation_defined()) return false;
-		$translation = static::$translation_key;
-		return $key ? @static::$$translation[$key] : static::$$translation;
+		return $key ? @static::$translation[$key] : static::$translation;
 	}
 
 
 	/**
-	 * 翻译或者编码属性
+	 * 翻译或反翻译属性
 	 * */
 	public function translateProperty ($key, $val=null)
 	{
@@ -48,14 +42,14 @@ trait Translater {
 
 
 	/**
-	 * [level-3] 转化指定的Model属性
-	 * 根据prop转化指定属性，prop=空，则转化翻译所有属性；$translate中未指定的属性将被忽略。
+	 * 转化指定的或所有的Model属性
+	 * 根据prop转化指定属性，prop is null，则转化翻译所有属性；$translate中未指定的属性将被忽略。
 	 * @prop mixed string|array， string：需要被翻译的属性，分隔符为','
 	 * @prf 转化属性名前缀，默认为'_'
 	 * @ext 转化属性名后缀，默认为''
 	 * @return 成功true；如果Model未指定translate属性名，则返回false。
 	 * */
-	public function translateToNewProperty($prop=null, $prf='_', $ext=''):bool
+	public function translatePropertyList ($prop=null, $prf='_', $ext=''):bool
 	{
 
 		if ( !self::translation_defined()) return false;
