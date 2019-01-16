f281e3eb5f42601c358716a7b143e6b384451777
Kane <52653784@qq.com>
2019-01-16 17:48:22 +0800

    # 跨域中间件优化：增加自定义配置方法

--- src/Laravel/Http/Middleware/CrossHttp.php
+++ src/Laravel/Http/Middleware/CrossHttp.php
@@ -14,11 +14,34 @@ class CrossHttp
 	public function handle (Request $req, \Closure $next)
 	{
 		$res = $next($req);
-//		$res->header('Access-Control-Allow-Origin', 'http://crm.wechat.9peak.net');
-		$res->header('Access-Control-Allow-Origin', '*');
-		$res->header('Access-Control-Allow-Header', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
-		$res->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-		$res->header('Access-Control-Allow-Credentials', 'false');
+
+		// 获取当前域名
+		$domain = $req->getHost();
+
+		// 获取域名配置
+		self::$domain = @self::$domain[$domain] ?: @self::$domain['*'];
+
+		// 设置Header
+		if (self::$domain) {
+			$res->header('Access-Control-Allow-Origin', $domain);
+			foreach (self::$domain as $key=>$val) {
+				$res->header('Access-Control-Allow-'.ucfirst($key), $val);
+			}
+		}
+
 		return $res;
 	}
+
+
+	/**
+	 * 设置域名
+	 * @param $domain array 多维数组，域名url(不含http:///)作键名，值是拥有Header、Methods等键值对组成的数组；
+	 * */
+	static function init (array $domain=[])
+	{
+		self::$domain = $domain;
+	}
+
+
+
 }
