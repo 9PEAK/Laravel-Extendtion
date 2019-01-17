cc71ef09947a3998be5e086041aa0377381b3d5b
Kane <52653784@qq.com>
2019-01-17 09:46:02 +0800

    # 跨域中间件Bug检测

--- src/Laravel/Http/Middleware/CrossHttp.php
+++ src/Laravel/Http/Middleware/CrossHttp.php
@@ -9,39 +9,27 @@ use Illuminate\Http\Request;
 class CrossHttp
 {
 
-	protected static $domain = [];
-
 	public function handle (Request $req, \Closure $next)
 	{
 		$res = $next($req);
 
-		// 获取当前域名
-		$domain = $req->getHost();
-
-		// 获取域名配置
-		self::$domain = @self::$domain[$domain] ?: @self::$domain['*'];
-		$domain = '*';
-		// 设置Header
-		if (self::$domain) {
-			$res->header('Access-Control-Allow-Origin', $domain);
-			foreach (self::$domain as $key=>$val) {
-				$res->header('Access-Control-Allow-'.ucfirst($key), $val);
-			}
+		foreach (self::$config as $key=>$val) {
+			$res->header('Access-Control-Allow-'.ucfirst($key), $val);
 		}
 
 		return $res;
 	}
 
-
 	/**
-	 * 设置域名
-	 * @param $domain array 多维数组，域名url(不含http:///)作键名，值是拥有Header、Methods等键值对组成的数组；
+	 * 设置参数
 	 * */
-	static function init (array $domain=[])
-	{
-		self::$domain = $domain;
-	}
 
+	protected static $config = [];
+
+	static function config (array $config=[])
+	{
+		self::$config = $config;
+	}
 
 
 }
